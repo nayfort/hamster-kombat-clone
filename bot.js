7ba55d3961da78ebd01dbd02fc1066eaaf23a250
nayfort <mitrofanovvladislav2004@gmail.com>
2024-08-08 23:54:37 +0300

    Initial commit

---
+++ bot.js
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv';
+import TelegramBot from 'node-telegram-bot-api';
+
+dotenv.config();
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const webAppUrl = 'https://hamster-kombat-clone.vercel.app';
+
+    console.log(`Send SMS to chat ${chatId} with button to open app: ${webAppUrl}`);
+
+    const options = {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: "Open app",
+                        web_app: { url: webAppUrl }
+                    }
+                ]
+            ]
+        }
+    };
+
+    bot.sendMessage(chatId, "Welcome to Hamster Kombat Clone Telegram bot! Press button to open!", options)
+        .then(response => {
+            console.log(`Message send successfully: ${response}`);
+        })
+        .catch(error => {
+            console.error(`Error send Message: ${error}`);
+        });
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    console.log(`Message received: ${msg.text}`);
+    bot.sendMessage(chatId, 'Your message has been received')
+        .then(response => {
+            console.log(`Reply sent successfully: ${response}`);
+        })
+        .catch(error => {
+            console.error(`Error sending response: ${error}`);
+        });
+});
---
+++ src/images/index.ts
@@ -0,0 +1,19 @@
+import exchange from "./exchange.png";
+import binance from "./binance.png";
+import dollar from "./dollar.png";
+import reward from "./reward.png";
+import cipher from "./cipher.png";
+import combo from "./combo.png";
+import hamster from "./hamster.png";
+import coin from "./coin.png";
+
+export {
+    exchange,
+    binance,
+    dollar,
+    reward,
+    cipher,
+    combo,
+    hamster,
+    coin
+};
